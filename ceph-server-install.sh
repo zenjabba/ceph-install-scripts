6d265becd2e55518784ca5468d5c54f7a546f424
zenjabba <zenjabba@users.noreply.github.com>
2021-02-06 12:18:33 -0500

    Update ceph-server-install.sh

--- ceph-server-install.sh
+++ ceph-server-install.sh
@@ -11,7 +11,6 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y
 sudo apt-get update -y
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io -y
-sudo usermod -aG docker seed
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose

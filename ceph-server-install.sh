94c65d73ce189294a108e3843cd88904ca4399ae
zenjabba <zenjabba@users.noreply.github.com>
2021-02-11 11:29:45 -0500

    Update ceph-server-install.sh

--- ceph-server-install.sh
+++ ceph-server-install.sh
@@ -17,12 +17,12 @@ sudo /usr/local/bin/ubuntu-mainline-kernel.sh -i 5.10.1 --yes
 
 
 ##docker
-sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y
-sudo apt-get update -y
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io -y
-sudo apt-get upgrade -y 
+#sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+#sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y
+#sudo apt-get update -y
+#sudo apt-get install -y docker-ce docker-ce-cli containerd.io -y
+#sudo apt-get upgrade -y 
 
 ##install jumpcloud
 curl --tlsv1.2 --silent --show-error --header 'x-connect-key: 64da8e09f6c0f4f9b863a96aae2fec356a1da795' https://kickstart.jumpcloud.com/Kickstart | sudo bash
